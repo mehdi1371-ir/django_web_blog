9b15d4b68834bdcef96ab94c61f765bcc0f469bb
mehdi <mhdpourbaba@gmail.com>
2022-03-18 19:06:57 +0330

    add post list view

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list_view, name='home')
+    path('', views.post_list_view, name='home'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
 from .models import Post
 
 
 def post_list_view(request):
     post_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
     return render(request, 'pages/posts_list.html', {'post_list': post_list})
+
+
+def post_detail_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'pages/post_detail.html', {'post': post})
