c15954317571987a6546d0445e225d739c485df2
mehdi <mhdpourbaba@gmail.com>
2022-03-18 14:26:04 +0330

    create post model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'published'),
+        ('drf', 'draft'),
+    )
+
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3, default='pub')
+
+    def __str__(self):
+        return self.title
+
