22f77cba706160ec013f7c35567b40ca08ccf50f
mehdi <mhdpourbaba@gmail.com>
2022-03-18 19:54:17 +0330

    add post create

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list_view(request):
@@ -12,3 +13,14 @@ def post_list_view(request):
 def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'pages/post_detail.html', {'post': post})
+
+
+def post_create_view(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'pages/post_create.html')
