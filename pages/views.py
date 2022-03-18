5cee43b44b3b4deac50c45098b6165d0a38c71ed
mehdi <mhdpourbaba@gmail.com>
2022-03-19 01:07:39 +0330

    add post delete feature

--- pages/views.py
+++ pages/views.py
@@ -35,3 +35,11 @@ def post_update_view(request, pk):
         return redirect('home')
     return render(request, 'pages/post_create.html', {'form': form})
 
+
+def post_delete_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        post.delete()
+        return redirect('home')
+    return render(request, 'pages/post_delete.html', {'post': post})
